eacb18834f0f89c3c904c684a9866896b3f642f2
Karin Patenge <karin.patenge@oracle.com>
2025-05-06 18:15:19 +0200

    Data and script added for first demo

--- 2025/05_APEX Connect/README.md
+++ 2025/05_APEX Connect/README.md
@@ -1,5 +1,32 @@
-# General information
+# APEX Connect 2025
+
+## General information
 
 [APEX Connect 2025](https://apex.doag.org/en/apex-connect-2025/) in Europa-Park Rust/DE from May 13-15, 2025.
 
 The conference program can be found [here](https://my.doag.org/events/apex-connect/2025/agenda/#eventDay.all).
+
+## Demos
+
+### Simplified creation of spatial data (vector data representing points, lines, polygons, or combinations of those; aka geometries or simple features)
+
+#### Scripts
+
+* [Create point geometries based on lon/lat values](./scripts/01_create_point_geometries_from_lon_lat.sql) using an open [data set containing information about street lamps in Nuremberg](./data/01_DE-BY-Nurnberg-202503200800.lit.csv), and published via the [GovData Portal Germany](https://www.govdata.de/suche/daten/strassenlampen-nurnberg-de-by).
+* [Typical queries queries for point geometries](./scripts/02_query_point_geometries.sql)
+
+### Load spatial data using Spatial Studio
+
+#### Scripts
+
+* [Load GeoJSON](./)
+* [Load from Web Feature Service](./)
+* [Load raster data](./)
+
+### Vector Tiles for large spatial data sets
+
+#### Scripts
+
+* [Create vector tiles](./20_create_vector_tiles_from_point_geometries.sql)
+* [Vector tiles caching](./21_cache_vector_tiles.sql)
+* [](./22_create_h3_for_hierarchical_aggregates_as_vector_tiles.sql)
---
+++ 2025/05_APEX Connect/scripts/01_create_point_geometries_from_lon_lat.sql
@@ -0,0 +1,51 @@
+/*********************************************
+ * Create point geometries from lon/lat values
+ *
+ * Author: Karin Patenge
+ * Date: May 2025
+ *********************************************/
+
+-- Import the csv data set using APEX, SQL Developer, SQLcl, or another tool of your choice
+-- The table name I choose is: govdata_street_lamps_nue
+
+-- Add a column to convert lon/lat into SDO_GEOMETRY
+ALTER TABLE govdata_street_lamps_nue ADD (geometry SDO_GEOMETRY);
+
+-- Fill the SDO_GEOMETRY column
+UPDATE govdata_street_lamps_nue
+SET geometry = SDO_GEOMETRY(longitude, latitude);
+
+COMMIT;
+
+-- Show the data
+SELECT
+  longitude,
+  latitude,
+  crs,
+  lit,
+  name,
+  osm,
+  geometry,
+  -- derived data: DOT notation for data type SDO_GEOMETRY
+  t.geometry.sdo_srid AS geom_srid,
+  t.geometry.sdo_gtype AS geom_gtype,
+  t.geometry.sdo_point.x AS geom_lon,
+  t.geometry.sdo_point.y AS geom_lat
+FROM
+  govdata_street_lamps_nue t;
+
+-- Check SDO metadata - will be created upon spatial index creation
+SELECT * FROM USER_SDO_GEOM_METADATA;
+
+-- Create spatial index (optimized index for point geometries)
+CREATE INDEX govdata_street_lamps_nue_sidx
+ON govdata_street_lamps_nue (geometry)
+INDEXTYPE IS MDSYS.SPATIAL_INDEX_V2 PARAMETERS ('layer_gtype=POINT');
+
+-- If needed, drop the index
+DROP INDEX govdata_street_lamps_nue_sidx FORCE;
+
+-- Re-check SDO metadata after spatial index creation
+SELECT * FROM USER_SDO_GEOM_METADATA;
+
+
