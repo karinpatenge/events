d0083b078e106c7ddc0168501477d6eb4f0f776e
Karin Patenge <karin.patenge@oracle.com>
2026-05-18 15:03:24 +0200

    Unused prefixes removed

--- 2026/05_APEX_Connect/02a-visualize_students_graph_query.sql
+++ 2026/05_APEX_Connect/02a-visualize_students_graph_query.sql
@@ -1,10 +1,20 @@
+-- Query the graph:
+-- Who (p1) is friend (f) of a person (p2) that studied (e) either mathematics (v) or studied at (e) the University of Hannover (v)?
+-- Return the names of the persons that match the criteria.
 SELECT *
-FROM GRAPH_TABLE (
-  ac_students_graph
-  MATCH (v1)-[e]->(v2)
-  COLUMNS (
-    vertex_id(v1) AS v1_node_id,
-    edge_id(e) AS e_edge_id,
-    vertex_id(v2) AS v2_node_id
+FROM
+  GRAPH_TABLE (
+    students_graph
+    MATCH (p1 IS person) -[f IS is_friend_of]-> (p2 IS person)-[e IS studied|studied_at]-> (v)
+    WHERE v.name = 'Mathematics' OR v.name = 'University of Hannover'
+    COLUMNS (
+      p1.name AS person_name,
+      vertex_id(p1) AS p1_node_id,
+      edge_id(f) AS f_edge_id,
+      vertex_id(p2) AS p2_node_id,
+      edge_id(e) AS e_edge_id,
+      vertex_id(v) AS v_node_id
+    )
   )
-)
+ORDER BY
+  person_name
